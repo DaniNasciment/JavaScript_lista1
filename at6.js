0d0a1b1bd0e07199ae90ae7733f382049913e8ad
Daniela Nascimento <112710222+DaniNasciment@users.noreply.github.com>
2022-09-11 22:27:48 -0300

    Add files via upload

---
+++ at6.js
@@ -0,0 +1,24 @@
+const prompt = require('prompt-sync')()
+
+const x1String = prompt('\n Informe o valor de x1')
+
+const x1 = Number(x1String)
+
+const y1String = prompt('\n Informe o valor de y1')
+
+const y1 = Number(y1String)
+
+const x2String = prompt('\n Informe o valor de x2')
+
+const x2 = Number(x2String)
+
+const y2String = prompt('\n Informe o valor de y2')
+
+const y2 = Number(y2String)
+
+const d = (Math.pow(x2-x1)+Math.pow(y2-y1))
+const d2 = Math.pow(d)
+
+console.log(`\n A distancia entre eles é:${d2}`)
+
+
