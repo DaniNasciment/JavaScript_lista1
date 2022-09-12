7b9b1761b260967531ec998f19c7d95af842e9fe
Daniela Nascimento <112710222+DaniNasciment@users.noreply.github.com>
2022-09-11 22:44:16 -0300

    Add files via upload

---
+++ at8.js
@@ -0,0 +1,13 @@
+const prompt = require('prompt-sync')()
+
+const custoFString = prompt('\n Informe o custo de fábrica:')
+
+const custoF = Number(custoFString)
+
+const pDistribuidor=custoF*0.28
+
+const imp=custoF*0.45
+
+const custoC=custoF+pDistribuidor+imp
+
+console.log(`\n O custo ao consumidor é em reais: ${custoC}`)
