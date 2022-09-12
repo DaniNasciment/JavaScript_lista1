9108f456fd2d55889791ad579988be1336afdff5
Daniela Nascimento <112710222+DaniNasciment@users.noreply.github.com>
2022-09-11 21:27:03 -0300

    Add files via upload

---
+++ at2.js
@@ -0,0 +1,17 @@
+const prompt = require('prompt-sync')()
+
+const diasString = prompt('Informe sua idade em dias ')
+
+const dias = Number(diasString)
+
+const anos= (dias/365)
+
+console.log(`\n Sua idade em anos é: ${anos}`)
+
+const meses =(dias%365)/30
+
+console.log(`\n Sua idade em meses é: ${meses}`)
+
+const dias2 = (dias%365)%30
+
+console.log(`\n Sua idade em dias é: ${dias2}`)
