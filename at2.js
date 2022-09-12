2243a745dd9b236b7636b04882a500a370f6d14e
Daniela Nascimento <112710222+DaniNasciment@users.noreply.github.com>
2022-09-12 11:03:06 -0300

    Update at2.js

--- at2.js
+++ at2.js
@@ -8,10 +8,10 @@ const anos= (dias/365);
 
 console.log(`\n Sua idade em anos é: ${anos}`);
 
-const meses =(dias%365)/30
+const meses =(dias%365)/30;
 
 console.log(`\n Sua idade em meses é: ${meses}`);
 
-const dias2 = (dias%365)%30
+const dias2 = (dias%365)%30;
 
 console.log(`\n Sua idade em dias é: ${dias2}`);
