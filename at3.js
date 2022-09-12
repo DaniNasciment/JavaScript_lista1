d053dda80814e431a101f3287191510b35bfea30
Daniela Nascimento <112710222+DaniNasciment@users.noreply.github.com>
2022-09-12 11:02:34 -0300

    Update at3.js

--- at3.js
+++ at3.js
@@ -1,17 +1,17 @@
-const prompt = require('prompt-sync')()
+const prompt = require('prompt-sync')();
 
-const segundosString = prompt('Informe o tempo de duração de um evento em uma fábrica expressa em segundos :')
+const segundosString = prompt('Informe o tempo de duração de um evento em uma fábrica expressa em segundos :');
 
-const segundos = Number(segundosString)
+const segundos = Number(segundosString);
 
-const horas = (segundos/3600)
+const horas = (segundos/3600);
 
-console.log(`\n A duração do evento em horas é: ${horas}`)
+console.log(`\n A duração do evento em horas é: ${horas}`);
 
-const minutos = (segundos%3600)/60
+const minutos = (segundos%3600)/60;
 
-console.log(`\n A duração do evento em minutos é: ${minutos}`)
+console.log(`\n A duração do evento em minutos é: ${minutos}`);
 
-const segundos2 = (segundos%3600)%60
+const segundos2 = (segundos%3600)%60;
 
-console.log(`\n A duração do evento em segundos é: ${segundos2}`)
+console.log(`\n A duração do evento em segundos é: ${segundos2}`);
