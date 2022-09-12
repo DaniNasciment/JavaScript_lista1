cda48eb797045af6ad880bbfd3dade900fbab71b
Daniela Nascimento <112710222+DaniNasciment@users.noreply.github.com>
2022-09-12 11:00:46 -0300

    Update at2.js

--- at2.js
+++ at2.js
@@ -1,17 +1,17 @@
-const prompt = require('prompt-sync')()
+const prompt = require('prompt-sync')();
 
-const diasString = prompt('Informe sua idade em dias ')
+const diasString = prompt('Informe sua idade em dias ');
 
-const dias = Number(diasString)
+const dias = Number(diasString);
 
-const anos= (dias/365)
+const anos= (dias/365);
 
-console.log(`\n Sua idade em anos é: ${anos}`)
+console.log(`\n Sua idade em anos é: ${anos}`);
 
 const meses =(dias%365)/30
 
-console.log(`\n Sua idade em meses é: ${meses}`)
+console.log(`\n Sua idade em meses é: ${meses}`);
 
 const dias2 = (dias%365)%30
 
-console.log(`\n Sua idade em dias é: ${dias2}`)
+console.log(`\n Sua idade em dias é: ${dias2}`);
