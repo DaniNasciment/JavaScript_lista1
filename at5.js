5043f924ba655d743f741a697eb5173ad28e2b88
Daniela Nascimento <112710222+DaniNasciment@users.noreply.github.com>
2022-09-11 22:16:07 -0300

    Add files via upload

---
+++ at5.js
@@ -0,0 +1,17 @@
+const prompt = require('prompt-sync')()
+
+const nota1String = prompt('\nInforme a primeira nota:')
+
+const nota1 = Number(nota1String)
+
+const nota2String = prompt('\nInforme a segunda nota:')
+
+const nota2 = Number(nota2String)
+
+const nota3String = prompt('\nInforme a terceira nota:')
+
+const nota3 = Number(nota3String)
+
+const media=(((nota1*2)+(nota2*3)+(nota3*5))/(nota1+nota2+nota3))
+
+console.log(`\n Sua média ponderada é: ${media}`)
