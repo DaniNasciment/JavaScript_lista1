ba8c2c7c5c45c98c1b5cea7f0a4a7eca6634a653
Daniela Nascimento <112710222+DaniNasciment@users.noreply.github.com>
2022-09-12 11:05:14 -0300

    Update at5.js

--- at5.js
+++ at5.js
@@ -1,17 +1,17 @@
-const prompt = require('prompt-sync')()
+const prompt = require('prompt-sync')();
 
-const nota1String = prompt('\nInforme a primeira nota:')
+const nota1String = prompt('\nInforme a primeira nota:');
 
-const nota1 = Number(nota1String)
+const nota1 = Number(nota1String);
 
-const nota2String = prompt('\nInforme a segunda nota:')
+const nota2String = prompt('\nInforme a segunda nota:');
 
-const nota2 = Number(nota2String)
+const nota2 = Number(nota2String);
 
-const nota3String = prompt('\nInforme a terceira nota:')
+const nota3String = prompt('\nInforme a terceira nota:');
 
-const nota3 = Number(nota3String)
+const nota3 = Number(nota3String);
 
-const media=(((nota1*2)+(nota2*3)+(nota3*5))/(nota1+nota2+nota3))
+const media=(((nota1*2)+(nota2*3)+(nota3*5))/(nota1+nota2+nota3));
 
-console.log(`\n Sua média ponderada é: ${media}`)
+console.log(`\n Sua média ponderada é: ${media}`);
