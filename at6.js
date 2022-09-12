5dae88f160adc3465f78e677d46c70c9717a4900
Daniela Nascimento <112710222+DaniNasciment@users.noreply.github.com>
2022-09-12 11:05:57 -0300

    Update at6.js

--- at6.js
+++ at6.js
@@ -1,24 +1,25 @@
-const prompt = require('prompt-sync')()
+const prompt = require('prompt-sync')();
 
-const x1String = prompt('\n Informe o valor de x1')
+const x1String = prompt('\n Informe o valor de x1');
 
-const x1 = Number(x1String)
+const x1 = Number(x1String);
 
-const y1String = prompt('\n Informe o valor de y1')
+const y1String = prompt('\n Informe o valor de y1');
 
-const y1 = Number(y1String)
+const y1 = Number(y1String);
 
-const x2String = prompt('\n Informe o valor de x2')
+const x2String = prompt('\n Informe o valor de x2');
 
-const x2 = Number(x2String)
+const x2 = Number(x2String);
 
-const y2String = prompt('\n Informe o valor de y2')
+const y2String = prompt('\n Informe o valor de y2');
 
-const y2 = Number(y2String)
+const y2 = Number(y2String);
 
-const d = (Math.pow(x2-x1)+Math.pow(y2-y1))
-const d2 = Math. sqrt(d)
+const d = (Math.pow(x2-x1)+Math.pow(y2-y1));
 
-console.log(`\n A distancia entre eles é:${d2}`)
+const d2 = Math. sqrt(d);
+
+console.log(`\n A distancia entre eles é:${d2}`);
 
 
