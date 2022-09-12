ae2a051ffbfccacb15029d3e84185a01572c5804
Daniela Nascimento <112710222+DaniNasciment@users.noreply.github.com>
2022-09-12 07:59:00 -0300

    Update at6.js

--- at6.js
+++ at6.js
@@ -17,7 +17,7 @@ const y2String = prompt('\n Informe o valor de y2')
 const y2 = Number(y2String)
 
 const d = (Math.pow(x2-x1)+Math.pow(y2-y1))
-const d2 = Math.pow(d)
+const d2 = Math. sqrt(d)
 
 console.log(`\n A distancia entre eles é:${d2}`)
 
