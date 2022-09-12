1bffcb290b7d6f0d9b0a8f82c40f4a9f6775910a
Daniela Nascimento <112710222+DaniNasciment@users.noreply.github.com>
2022-09-11 22:36:46 -0300

    Add files via upload

---
+++ at7.js
@@ -0,0 +1,33 @@
+const prompt = require('prompt-sync')()
+
+const aString = prompt('\n Informe o valor de a :')
+
+const a = Number(aString)
+
+
+const bString = prompt('\n Informe o valor de b :')
+
+const b = Number(bString)
+
+
+const cString = prompt('\n Informe o valor de c :')
+
+const c = Number(cString)
+
+
+const dString = prompt('\n Informe o valor de d :')
+
+const d = Number(dString)
+
+const eString = prompt('\n Informe o valor de e :')
+
+const e = Number(eString)
+
+const fString = prompt('\n Informe o valor de f :')
+
+const f = Number(fString)
+
+const x=((c * e) - (b * f)) / ((a * e) - (b * d))
+const y=((a * f) - (c * d)) / ((a * e) - (b * d))
+
+console.log(`\n O valor de x e y é, respectivamente: ${x} , ${y}`)
