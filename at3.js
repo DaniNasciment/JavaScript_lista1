5bace78fd1302bf04d86f2093d4670e97f61aebc
Daniela Nascimento <112710222+DaniNasciment@users.noreply.github.com>
2022-09-11 21:39:47 -0300

    Add files via upload

---
+++ at3.js
@@ -0,0 +1,17 @@
+const prompt = require('prompt-sync')()
+
+const segundosString = prompt('Informe o tempo de duração de um evento em uma fábrica expressa em segundos :')
+
+const segundos = Number(segundosString)
+
+const horas = (segundos/3600)
+
+console.log(`\n A duração do evento em horas é: ${horas}`)
+
+const minutos = (segundos%3600)/60
+
+console.log(`\n A duração do evento em minutos é: ${minutos}`)
+
+const segundos2 = (segundos%3600)%60
+
+console.log(`\n A duração do evento em segundos é: ${segundos2}`)
